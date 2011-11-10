2cd282cf37af9d1a249f9ab42ee68dfd7c407aea
Eric Wood <eric@woodtx.com>
2011-11-10 12:18:52 -0600

    Good enough, pushing.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,17 +5,21 @@ function preserve_stuff
 {
 	if [ -h ~/$1 ]; then
 		echo "~/$1 already exists, removing symlink"
-		#rm ~/$1
+		rm ~/$1
 	elif [ -a ~/$1 ]; then
 		echo "~/$1 already exists, moving to: ~/$1_old"
-		#mv ~/$1 ~/$1_old
+		mv ~/$1 ~/$1_old
 	fi
-	#ln -s $PWD/$1 ~/$1
+	ln -s $PWD/$1 ~/$1
 }
 
 # Glob all dotfiles with a few exceptions
 # Now I don't have to update this script much in the future!
-preserve_stuff !(.git|.gitignore|*.swp|README.md|bootstrap.sh|.bash_shenanigans|.irssi)
+#preserve_stuff !(.git|.gitignore|*.swp|README.md|bootstrap.sh|.bash_shenanigans|.irssi)
+preserve_stuff .screenrc
+preserve_stuff .tmux.conf
+preserve_stuff .vim
+preserve_stuff .vimrc
 
 # irssi stuff is a different matter, though
 preserve_stuff .irssi/config
